21dd9b99b75dafb6eeb8754108e73b83acc1076c
YashwantKora <Yashwantqwerty@gmail.com>
2025-02-09 13:16:36 +0530

    BS DONE

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,13 +1,16 @@
-# def BubbleSort(data):
-#     for i in range(len(data)):
-#         for j in range(len(data) - i - 1):
-#             if data[j] > data[j + 1]:
-#                 temp = data[j]
-#                 data[j], data[j + 1] = data[j + 1], data[j]
+# Start with an array of numbers.
+# Repeat for the length of the array:
+
+#     For each element in the array (except the last sorted elements):
+#         Compare the current element with the next one.
+#         If the current element is greater than the next one, swap them.
+
+# If no swaps were made in a full pass, the array is sorted.
+# End when the array is sorted.
 
 def BubbleSort(data):
     n = len(data)
-    for i in range(n):
+    for i in range(n - 1):
         for j in range(n -i -1):
             if data[j] > data[j + 1]:
                 data[j], data[j + 1] = data[j + 1], data[j]
