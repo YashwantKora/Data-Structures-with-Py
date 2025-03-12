1476888be5cce553888bd10dc0c1198d4fe5a58b
YashwantKora <Yashwantqwerty@gmail.com>
2025-03-12 22:01:20 +0530

    Factorial

--- factorial.py
+++ factorial.py
@@ -1,11 +1,21 @@
 import sys
 sys.setrecursionlimit(10**6)
+
+# def factorial(n):
+#     if n < 0 and int(n) != n:
+#       print("not valid bro <insert skull emoji>")
+#     if n == 1 or n == 0:
+#         return 1
+#     else:
+#        return n*factorial(n-1)
+
 def factorial(n):
     if n < 0 and int(n) != n:
-      print("NA")
+        print("NoT VAliD")
     if n == 1 or n == 0:
         return 1
     else:
-       return n*factorial(n-1)
+        return n * factorial(n-1)
+    
 f = int(input("ENTER YOUR NUMBER: "))
 print(factorial(f))
