c1f62fe0e8a5be24e3d4e1ce621550286287ae57
YashwantKora <Yashwantqwerty@gmail.com>
2025-03-12 21:59:51 +0530

    Parantheses

--- parantheses.py
+++ parantheses.py
@@ -2,21 +2,37 @@ open_list = ["[", "{", "("]
 close_list = ["]", "}", ")"]
 
 
-def check_parantheses(my_str):
-    Stack = []
-    for i in open_list:
+# def check_parantheses(string):
+#     Stack = []
+#     for i in string:
+#         if i in open_list:
+#             Stack.append(i)
+#         elif i in close_list:
+#             pos = close_list.index(i)
+#             if len(Stack) > 0 and open_list[pos] == Stack[len(Stack) - 1]:
+#                 Stack.pop()
+#             else:
+#                 return "UNBALANCED"
+#     if len(Stack) == 0:
+#         return "BALANCED"
+#     else:
+#         return "UNBALANCED"
+
+def check_parantheses(string):
+    stack = []
+    for i in string:
         if i in open_list:
-            Stack.append(i)
+            stack.append(i)
         elif i in close_list:
             pos = close_list.index(i)
-            if len(Stack) > 0 and open_list[pos] == Stack[len(Stack) - 1]:
-                Stack.pop()
+            if len(stack) > 0 and open_list[pos] == stack[len(stack) -1]:
+                stack.pop()
             else:
-                return "UNBALANCED"
-    if len(Stack) == 0:
-        return "BALANCED"
+                return "Unbalanced"
+    if len(stack) == 0:
+        return "Balanced"
     else:
-        return "UNBALANCED"
+        return "Unbalanced"
     
 string = "{[]{()}}"
 print(string, check_parantheses(string))
