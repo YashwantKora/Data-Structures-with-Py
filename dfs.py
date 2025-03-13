eec1cc752b9942cbc32287008537d89c9f14383e
YashwantKora <Yashwantqwerty@gmail.com>
2025-03-13 06:30:31 +0530

    new

--- dfs.py
+++ dfs.py
@@ -1,18 +1,34 @@
 graph = {'a':['b','d','e','f'],'d':['a'],'b':['a','f','c'],'f':['b','a'],'c':['b'],'e':['a']}
 print(graph)
 
-def dfs(graph, source):
-    Stack = list()
-    visited_list = list()
-    Stack.append(source)
-    visited_list.append(source)
+# def dfs(graph, source):
+#     Stack = list()
+#     visited_list = list()
+#     Stack.append(source)
+#     visited_list.append(source)
 
-    while Stack:
-        vertex = Stack.pop()
+#     while Stack:
+#         vertex = Stack.pop()
+#         print(vertex, end=" ")
+
+#         for i in graph:
+#             if i not in visited_list:
+#                 Stack.append(i)
+#                 visited_list.append(i)
+
+def dfs(graph, source):
+    stack = []
+    visited = []
+    stack.append(source)
+    visited.append(source)
+
+    while stack:
+        vertex = stack.pop()
         print(vertex, end=" ")
 
-        for visited in graph:
-            if visited not in visited_list:
-                Stack.append(visited)
-                visited_list.append(visited)
+        for i in graph:
+            if i not in visited:
+                stack.append(i)
+                visited.append(i)
+
 dfs(graph, "a")
