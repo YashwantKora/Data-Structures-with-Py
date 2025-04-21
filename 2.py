86a22ea2ca9987e681f5361dba209a3e4b1bf4e0
YashwantKora <Yashwantqwerty@gmail.com>
2025-04-21 12:32:18 +0530

    #2

---
+++ 2.py
@@ -0,0 +1,17 @@
+str = "Hello, World!"
+i = 100
+f = 1.1
+common_num = 19j + 21
+list = [1, 1.7, 'damn']
+tuple = (1, 2, 3)
+dict = {1 : "name1", 2 : "name2"}
+set = {1, 2, 3, 4, 5}
+
+print(type(str))
+print(type(i))
+print(type(f))
+print(type(common_num))
+print(type(list))
+print(type(tuple))
+print(type(dict))
+print(type(set))
