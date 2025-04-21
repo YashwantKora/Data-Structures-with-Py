76f45f7fe3a651107fe902d8834a0988ef2a4214
YashwantKora <Yashwantqwerty@gmail.com>
2025-04-21 22:20:24 +0530

    new commits

--- 13.py
+++ 13.py
@@ -1,131 +1,51 @@
+#13 Implement Singly Linked List (traversing nodes, searching, prepending, removing nodes).
+
+
 class Node:
     def __init__(self, data):
         self.data = data
-        self.next = next
+        self.next = None
 
+class LinkedList:
+    def __init__(self):
         self.head = None
-
-    def insert_at_beginning(self, new_data):
-         new_node = Node(new_data)
-         new_node.next = self.head
-         self.head = new_node 
-
-    def delete_at_beginning(self):
-        if self.head is None:
-            print("SLL IS EMPTY")
-        else:
-             self.head = self.head.next
     
+    def insert_at_beginning(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete_beginning(self):
+        if self.head is None:
+            print("LL is Empty!")
+        else:
+            self.head = self.head.next
+        
     def search(self, key):
-        temp = self.head
-        while temp is not None:
-            if temp.data == key:
+        current = self.head
+        while current is not None:
+            if current.data == key:
                 return True
-            temp = temp.next
+            current = current.next
         return False
     
-    def print(self):
+    def printList(self):
         temp = self.head
         while temp:
-            print(temp.data)
+            print(str(temp.data) + " ", end="")
             temp = temp.next
 
-LL = LL()
-n = Node(5)
-LL.head = n
-n1 = Node(8)
-n.next = n1
+if __name__ == '__main__':
+    LL = LinkedList()
+    n = Node(1)
+    LL.head = n
+    n1 = Node(2)
+    n.next = n1
 
-LL.insert_at_beginning(10)
-LL.insert_at_beginning(20)
-#LL.insert_at_end(30)
-print("LL")
-LL.print()
-print("|")
-LL.delete_at_beginning()
-LL.print()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Node:
-#     def __init__(self, data):
-#         self.data = data
-#         self.next = None
-
-# class LL:
-#     def __init__(self):
-#         self.head = None
-
-#     def insert_at_beginning(self, new_data):
-#         new_node = Node(new_data)
-#         new_node.next = self.head
-#         self.head = new_node
-
-#     def insert_at_end(self, new_data):
-#         new_node = Node(new_data)
-#         if self.head is None:
-#             self.head = new_node
-#             return
-#         last = self.head
-#         while last.next:
-#             last = last.next
-#         last.next = new_node
-
-#     def delete_at_beginning(self):
-#         if self.head is None:
-#             print("SLL IS EMPTY")
-#         else:
-#             self.head = self.head.next
-
-#     def search(self, key):
-#         temp = self.head
-#         while temp is not None:
-#             if temp.data == key:
-#                 return True
-#             temp = temp.next
-#         return False
-    
-#     def print(self):
-#         temp = self.head
-#         while temp:
-#             print(temp.data)
-#             temp = temp.next
-
-# LL = LL()
-# n = Node(5)
-# LL.head = n
-# n1 = Node(8)
-# n.next = n1
-
-# LL.insert_at_beginning(10)
-# LL.insert_at_beginning(20)
-# LL.insert_at_end(30)
-# print("LL")
-# LL.print()
-# print("|")
-# LL.delete_at_beginning()
-# LL.print()
+    LL.insert_at_beginning(10)
+    LL.insert_at_beginning(20)
+    print("LL")
+    LL.printList()
+    print("|")
+    LL.delete_beginning()
+    LL.printList()
